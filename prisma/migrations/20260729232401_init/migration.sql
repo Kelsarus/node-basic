9f3538e5e230d38fd0121006143e096e36456a99
Kelsarus <riankelsen323@gmail.com>
2026-07-29 20:26:11 -0300

    migra banco para postgresql

---
+++ prisma/migrations/20260729232401_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "animes" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "episodios" INTEGER NOT NULL,
+
+    CONSTRAINT "animes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_email_key" ON "usuarios"("email");
